da50f8cb476a98176ede45fd640696a59e9bc9da
Sum1Code <nathanael.adrean@gmail.com>
2023-10-29 15:06:09 +0800

    added the raylib sources as example

--- CPM.h
+++ CPM.h
@@ -18,14 +18,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <time.h>
 
-#define GET_FILE_NAME(name) \
-  (strrchr(name, '/') ? strrchr(name, '/') + 1 : name)
+#define GET_FILE_NAME(name) (strrchr(name, '/') ? strrchr(name, '/') + 1 : name)
 
 #define KNRM "\x1B[0m"
 #define KRED "\x1B[31m"
@@ -36,30 +34,18 @@
 #define KCYN "\x1B[36m"
 #define KWHT "\x1B[37m"
 
-typedef enum
-{
-  CPM_LOG,
-  CPM_WARNING,
-  CPM_ERROR
-} LOG_LEVEL;
+typedef enum { CPM_LOG, CPM_WARNING, CPM_ERROR } LOG_LEVEL;
 
 #define CPMLOG(loglevel, msg) _cpmlog(loglevel, msg);
 
-typedef enum
-{
-  EXECUTABLE,
-  OBJECT,
-  DYNLIB
-} BuildType_e;
+typedef enum { EXECUTABLE, OBJECT, DYNLIB } BuildType_e;
 
-typedef enum
-{
+typedef enum {
   D_CREATE,
   D_DELETE,
 } DirOps_e;
 
-typedef struct
-{
+typedef struct {
   BuildType_e type;
   char *srcs;
   char *include_dir;
@@ -69,15 +55,13 @@ typedef struct
   char *build_dir;
 } BuildProperties_t;
 
-typedef struct StringBuilder
-{
+typedef struct StringBuilder {
   char *str;
   size_t strsize;
   size_t strcap;
 } StringBuilder_t;
 
-typedef struct StringSplice
-{
+typedef struct StringSplice {
   char **strsplice;
   char delim;
   size_t splice_count;
@@ -92,7 +76,8 @@ StringSplice_t *sb_split_at(StringBuilder_t *builder, char *delim);
 StringBuilder_t *sb_copy(StringBuilder_t *sb);
 
 void _cpmlog(LOG_LEVEL level, const char *msg);
-void cpm_target(BuildProperties_t *buildprop, BuildType_e type, const char *name, const char *build_dir);
+void cpm_target(BuildProperties_t *buildprop, BuildType_e type,
+                const char *name, const char *build_dir);
 void cpm_init(BuildProperties_t *buildprop, char *compiler);
 void _cpm_srcs_create(BuildProperties_t *buildprop, ...);
 void _cpm_cflags_create(BuildProperties_t *buildprop, ...);
@@ -102,42 +87,38 @@ bool shouldRecompile(char *srcfile, char *execfile);
 void cpm_compile(BuildProperties_t *prop);
 pid_t cpm_compile_async(BuildProperties_t *prop);
 void cpm_compile_async_poll(pid_t pid);
-void cpm_quick_compile(char* compiler, char* srcs, char* build_dir, BuildType_e target, char* name, char* flags);
-pid_t cpm_quick_compile_async(char* compiler, char* srcs, char* build_dir, BuildType_e target, char* name, char* flags);
+void cpm_quick_compile(char *compiler, char *srcs, char *build_dir,
+                       BuildType_e target, char *name, char *flags);
+pid_t cpm_quick_compile_async(char *compiler, char *srcs, char *build_dir,
+                              BuildType_e target, char *name, char *flags);
 
 static bool cpm_setup_warning_once = false;
 static bool cpm_setup_ran_once = false;
 
-
-#define cpm_compile_async_poll_many(pid_array)          \
-  for (size_t i = 0; i < ARRAY_SIZE(pid_array, pid_t); i++) \
-  {                                                     \
-    cpm_compile_async_poll(pid_array[i]);               \
+#define cpm_compile_async_poll_many(pid_array)                                 \
+  for (size_t i = 0; i < ARRAY_SIZE(pid_array, pid_t); i++) {                  \
+    cpm_compile_async_poll(pid_array[i]);                                      \
   }
 
-#define sb_append_strspace(string_builder_ptr, str) \
-  sb_appendstr(string_builder_ptr, str);            \
+#define sb_append_strspace(string_builder_ptr, str)                            \
+  sb_appendstr(string_builder_ptr, str);                                       \
   sb_append(string_builder_ptr, ' ');
 
-#define cpm_flags(buildprop_ptr, ...) \
+#define cpm_flags(buildprop_ptr, ...)                                          \
   _cpm_cflags_create(buildprop_ptr, __VA_ARGS__, NULL)
-#define cpm_srcs(buildprop_ptr, ...) \
+#define cpm_srcs(buildprop_ptr, ...)                                           \
   _cpm_srcs_create(buildprop_ptr, __VA_ARGS__, NULL)
 
 #define ARRAY_SIZE(array, type) (sizeof(array) / sizeof(type))
 
-
-
-void _cpmlog(LOG_LEVEL level, const char *msg)
-{
+void _cpmlog(LOG_LEVEL level, const char *msg) {
   time_t current_time;
   struct tm *time_info;
 
   time(&current_time);
   time_info = localtime(&current_time);
 
-  switch (level)
-  {
+  switch (level) {
   case CPM_LOG:
     printf("%sLOG (%02d:%02d:%02d): %s%s\n", KMAG, time_info->tm_hour,
            time_info->tm_min, time_info->tm_sec, msg, KNRM);
@@ -158,16 +139,14 @@ void _cpmlog(LOG_LEVEL level, const char *msg)
 }
 
 void cpm_target(BuildProperties_t *buildprop, BuildType_e type,
-                const char *name, const char *build_dir)
-{
+                const char *name, const char *build_dir) {
   buildprop->project_name = (char *)name;
   buildprop->type = type;
   buildprop->build_dir = (char *)build_dir;
 }
 
 #define STRING_ALLOC_SIZE 255
-void cpm_init(BuildProperties_t *buildprop, char *compiler)
-{
+void cpm_init(BuildProperties_t *buildprop, char *compiler) {
   buildprop->compiler = (char *)malloc(STRING_ALLOC_SIZE);
   buildprop->flags = (char *)malloc(STRING_ALLOC_SIZE);
   buildprop->include_dir = (char *)malloc(STRING_ALLOC_SIZE);
@@ -176,8 +155,7 @@ void cpm_init(BuildProperties_t *buildprop, char *compiler)
 
   if (buildprop->compiler == NULL || buildprop->flags == NULL ||
       buildprop->include_dir == NULL || buildprop->project_name == NULL ||
-      buildprop->srcs == NULL)
-  {
+      buildprop->srcs == NULL) {
     perror("Memory allocation error");
     exit(EXIT_FAILURE);
   }
@@ -190,18 +168,17 @@ void cpm_init(BuildProperties_t *buildprop, char *compiler)
   buildprop->srcs[0] = '\0';
 }
 
-void _cpm_srcs_create(BuildProperties_t *buildprop, ...)
-{
+void _cpm_srcs_create(BuildProperties_t *buildprop, ...) {
   va_list args;
   va_start(args, buildprop);
   char *arg = va_arg(args, char *);
   StringBuilder_t *builder = sb_new();
-  while (arg != NULL)
-  {
+  while (arg != NULL) {
     sb_appendstr(builder, arg);
     // char *newSrc = appendToString(buildprop->srcs, arg);
     // free(buildprop
-    //          ->srcs);         // Free the old srcs if they were dynamically allocated
+    //          ->srcs);         // Free the old srcs if they were dynamically
+    //          allocated
     // buildprop->srcs = newSrc; // Update srcs with the new concatenated string
     arg = va_arg(args, char *);
   }
@@ -209,14 +186,12 @@ void _cpm_srcs_create(BuildProperties_t *buildprop, ...)
   buildprop->srcs = sb_to_string(builder);
 }
 
-void _cpm_cflags_create(BuildProperties_t *buildprop, ...)
-{
+void _cpm_cflags_create(BuildProperties_t *buildprop, ...) {
   va_list args;
   va_start(args, buildprop);
   StringBuilder_t *resbuild = sb_new();
   char *arg = va_arg(args, char *);
-  while (arg != NULL)
-  {
+  while (arg != NULL) {
     sb_append_strspace(resbuild, arg);
     arg = va_arg(args, char *);
   }
@@ -224,19 +199,21 @@ void _cpm_cflags_create(BuildProperties_t *buildprop, ...)
   buildprop->flags = sb_to_string(resbuild);
 }
 
-void cpm_quick_compile(char* compiler, char* srcs, char* build_dir, BuildType_e target, char* name, char* flags){
+void cpm_quick_compile(char *compiler, char *srcs, char *build_dir,
+                       BuildType_e target, char *name, char *flags) {
   BuildProperties_t bp;
   cpm_init(&bp, compiler);
   if (!flags)
-  cpm_flags(&bp, " ");
-  else 
-  cpm_flags(&bp, flags);
+    cpm_flags(&bp, " ");
+  else
+    cpm_flags(&bp, flags);
   cpm_srcs(&bp, srcs);
   cpm_target(&bp, target, name, build_dir);
   cpm_compile(&bp);
 }
 
-pid_t cpm_quick_compile_async(char* compiler, char* srcs, char* build_dir, BuildType_e target, char* name, char* flags){
+pid_t cpm_quick_compile_async(char *compiler, char *srcs, char *build_dir,
+                              BuildType_e target, char *name, char *flags) {
   BuildProperties_t bp;
   cpm_init(&bp, compiler);
   cpm_flags(&bp, flags);
@@ -246,14 +223,13 @@ pid_t cpm_quick_compile_async(char* compiler, char* srcs, char* build_dir, Build
   return pid;
 }
 
-void sb_patsubst(StringBuilder_t *sb, const char *text_to_replace, const char *replacement)
-{
+void sb_patsubst(StringBuilder_t *sb, const char *text_to_replace,
+                 const char *replacement) {
   size_t text_len = strlen(text_to_replace);
   size_t replace_len = strlen(replacement);
 
   char *start = sb->str;
-  while ((start = strstr(start, text_to_replace)) != NULL)
-  {
+  while ((start = strstr(start, text_to_replace)) != NULL) {
     // Calculate the length of the portion before the match
     size_t prefix_len = start - sb->str;
 
@@ -261,7 +237,8 @@ void sb_patsubst(StringBuilder_t *sb, const char *text_to_replace, const char *r
     size_t suffix_len = sb->strsize - prefix_len - text_len;
 
     // Allocate a temporary buffer for the modified string
-    char *temp_buffer = (char *)malloc(prefix_len + replace_len + suffix_len + 1);
+    char *temp_buffer =
+        (char *)malloc(prefix_len + replace_len + suffix_len + 1);
 
     // Copy the prefix
     strncpy(temp_buffer, sb->str, prefix_len);
@@ -283,8 +260,7 @@ void sb_patsubst(StringBuilder_t *sb, const char *text_to_replace, const char *r
   }
 }
 
-StringBuilder_t *sb_copy(StringBuilder_t *sb)
-{
+StringBuilder_t *sb_copy(StringBuilder_t *sb) {
   StringBuilder_t *cpy = malloc(sizeof(StringBuilder_t));
   cpy->str = malloc(sb->strcap);
   cpy->strcap = sb->strcap;
@@ -293,18 +269,16 @@ StringBuilder_t *sb_copy(StringBuilder_t *sb)
   return cpy;
 }
 
-void cpm_compile(BuildProperties_t *prop)
-{
+void cpm_compile(BuildProperties_t *prop) {
   StringBuilder_t *sbuild = sb_new();
   sb_append_strspace(sbuild, prop->compiler);
   sb_append_strspace(sbuild, prop->flags);
-  switch (prop->type)
-  {
+  switch (prop->type) {
   case DYNLIB:
-    sb_append_strspace(sbuild, "-shared -fpic -c");
+    sb_append_strspace(sbuild, "-shared -fpic");
     break;
   case OBJECT:
-    sb_append_strspace(sbuild, "-c");
+    sb_append_strspace(sbuild, "-c -fpic");
     break;
   case EXECUTABLE:
     break;
@@ -322,15 +296,11 @@ void cpm_compile(BuildProperties_t *prop)
   system(cmdstr);
 }
 
-pid_t cpm_compile_async(BuildProperties_t *prop)
-{
+pid_t cpm_compile_async(BuildProperties_t *prop) {
   pid_t kawaii_child = fork();
-  if (kawaii_child == -1)
-  {
+  if (kawaii_child == -1) {
     CPMLOG(CPM_ERROR, "My children... No...: ");
-  }
-  else if (kawaii_child == 0)
-  {
+  } else if (kawaii_child == 0) {
     cpm_compile(prop);
     exit(0);
   }
@@ -338,38 +308,33 @@ pid_t cpm_compile_async(BuildProperties_t *prop)
   return kawaii_child;
 }
 
-void cpm_compile_async_poll(pid_t pid)
-{
+void cpm_compile_async_poll(pid_t pid) {
   int status;
   if (waitpid(pid, &status, 0) == -1)
     CPMLOG(CPM_ERROR, "I'm impatient! so is my child (waitpid error)");
-  if (WIFSIGNALED(status))
-  {
+  if (WIFSIGNALED(status)) {
     printf("Compilation terminated by signal: %d\n", WTERMSIG(status));
   }
 }
 
 #ifdef _WIN32
-char *cpm_glob_dir(char *dirpath, const char *pattern)
-{
-  CPMLOG(CPM_ERROR, "TODO: glob implementation for windows not available yet! sowwyy... >w<")
+char *cpm_glob_dir(char *dirpath, const char *pattern) {
+  CPMLOG(
+      CPM_ERROR,
+      "TODO: glob implementation for windows not available yet! sowwyy... >w<")
 }
 #else
-char *cpm_glob_dir(char *dirpath, const char *pattern)
-{
+char *cpm_glob_dir(char *dirpath, const char *pattern) {
   DIR *dir;
   struct dirent *entry;
   StringBuilder_t *resbuild = sb_new();
-  if ((dir = opendir(dirpath)) == NULL)
-  {
+  if ((dir = opendir(dirpath)) == NULL) {
     perror("opendir");
     return NULL;
   }
 
-  while ((entry = readdir(dir)) != NULL)
-  {
-    if (fnmatch(pattern, entry->d_name, 0) == 0)
-    {
+  while ((entry = readdir(dir)) != NULL) {
+    if (fnmatch(pattern, entry->d_name, 0) == 0) {
       sb_appendstr(resbuild, dirpath);
       sb_append(resbuild, '/');
       sb_appendstr(resbuild, entry->d_name);
@@ -382,41 +347,32 @@ char *cpm_glob_dir(char *dirpath, const char *pattern)
   return result;
 }
 #endif
-void cpm_dirops(DirOps_e operation, char *dirpath)
-{
+void cpm_dirops(DirOps_e operation, char *dirpath) {
   char command[256];
-  switch (operation)
-  {
+  switch (operation) {
   case D_CREATE:
 
     snprintf(command, sizeof(command), "mkdir -p %s", dirpath);
 
-    if (system(command) == 0)
-    {
+    if (system(command) == 0) {
       CPMLOG(CPM_LOG, command);
-    }
-    else
-    {
+    } else {
       CPMLOG(CPM_ERROR, "DIRECTORY CREATION FAILED!");
     }
     break;
   case D_DELETE:
     snprintf(command, sizeof(command), "rm -rf %s", dirpath);
 
-    if (system(command) == 0)
-    {
+    if (system(command) == 0) {
       CPMLOG(CPM_LOG, command);
-    }
-    else
-    {
+    } else {
       CPMLOG(CPM_ERROR, "DIRECTORY DELETION FAILED!");
     }
     break;
   }
 }
 
-bool shouldRecompile(char *srcfile, char *execfile)
-{
+bool shouldRecompile(char *srcfile, char *execfile) {
   struct stat sourceInfo, executableInfo;
 
   if (stat(srcfile, &sourceInfo) == -1)
@@ -428,28 +384,26 @@ bool shouldRecompile(char *srcfile, char *execfile)
   return sourceInfo.st_mtime > executableInfo.st_mtime;
 }
 
-#define cpm_rebuild_self(argv)                                           \
-  if (shouldRecompile(__FILE__, argv[0]))                                \
-  {                                                                      \
-    char *change_name_cmd = malloc(255);                                 \
-    sprintf(change_name_cmd, "mv %s %s.old", argv[0], argv[0]);          \
-    CPMLOG(CPM_WARNING, "changing current executable to old");           \
-    system(change_name_cmd);                                             \
-    CPMLOG(CPM_WARNING, "recompiling build system");                     \
-    BuildProperties_t prop;                                              \
-    cpm_init(&prop, "cc");                                               \
-    cpm_target(&prop, EXECUTABLE, GET_FILE_NAME(argv[0]), "./");         \
-    cpm_srcs(&prop, __FILE__);                                           \
-    cpm_compile(&prop);                                                  \
-    CPMLOG(CPM_WARNING, "RUNNING NEW BUILDER\n---------------------\n"); \
-    if (system(argv[0]) != 0)                                            \
-      CPMLOG(CPM_ERROR, "CANNOT RUN NEW BUILDER");                       \
-    exit(0);                                                             \
+#define cpm_rebuild_self(argv)                                                 \
+  if (shouldRecompile(__FILE__, argv[0])) {                                    \
+    char *change_name_cmd = malloc(255);                                       \
+    sprintf(change_name_cmd, "mv %s %s.old", argv[0], argv[0]);                \
+    CPMLOG(CPM_WARNING, "changing current executable to old");                 \
+    system(change_name_cmd);                                                   \
+    CPMLOG(CPM_WARNING, "recompiling build system");                           \
+    BuildProperties_t prop;                                                    \
+    cpm_init(&prop, "cc");                                                     \
+    cpm_target(&prop, EXECUTABLE, GET_FILE_NAME(argv[0]), "./");               \
+    cpm_srcs(&prop, __FILE__);                                                 \
+    cpm_compile(&prop);                                                        \
+    CPMLOG(CPM_WARNING, "RUNNING NEW BUILDER\n---------------------\n");       \
+    if (system(argv[0]) != 0)                                                  \
+      CPMLOG(CPM_ERROR, "CANNOT RUN NEW BUILDER");                             \
+    exit(0);                                                                   \
   }
 
 // SB IMPL
-StringBuilder_t *sb_new()
-{
+StringBuilder_t *sb_new() {
   StringBuilder_t *sb_res = malloc(sizeof(StringBuilder_t));
   if (!sb_res)
     CPMLOG(CPM_ERROR, "String builder failed to create");
@@ -459,12 +413,10 @@ StringBuilder_t *sb_new()
   return sb_res;
 }
 #define SB_VECTOR_LOAD_FACTOR 2
-void sb_append(StringBuilder_t *sb, char c)
-{
+void sb_append(StringBuilder_t *sb, char c) {
   sb->str[sb->strsize] = c;
   ++sb->strsize;
-  if (sb->strsize == sb->strcap)
-  {
+  if (sb->strsize == sb->strcap) {
     char *new_str = realloc(sb->str, sb->strcap * SB_VECTOR_LOAD_FACTOR);
     if (!new_str)
       CPMLOG(CPM_ERROR, "FAILED TO REALLOC STRING");
@@ -473,42 +425,39 @@ void sb_append(StringBuilder_t *sb, char c)
     sb->strcap *= 2;
   }
 }
-void sb_appendstr(StringBuilder_t *sb, char *str)
-{
-  for (size_t strsize = 0; strsize < strlen(str); strsize++)
-  {
+void sb_appendstr(StringBuilder_t *sb, char *str) {
+  for (size_t strsize = 0; strsize < strlen(str); strsize++) {
     sb_append(sb, str[strsize]);
   }
 }
-void sb_free(StringBuilder_t *sb)
-{
+void sb_free(StringBuilder_t *sb) {
   free(sb->str);
   free(sb);
 }
 
-/// WARNING: sb_retstr WILL RETURN THE STRING CONTAINED BUT CONSUME THE STRING BUILDER
-char *sb_to_string(StringBuilder_t *sb)
-{
-  char* ret = strdup(sb->str);
+/// WARNING: sb_retstr WILL RETURN THE STRING CONTAINED BUT CONSUME THE STRING
+/// BUILDER
+char *sb_to_string(StringBuilder_t *sb) {
+  char *ret = strdup(sb->str);
   sb_free(sb);
   return ret;
 }
 
 #define STRING_SPLICE_INIT_SIZE 5
-/// Borrows StringBuilder and create a unique StringSplice that should be immutable
-StringSplice_t *sb_split_at(StringBuilder_t *builder, char *delim)
-{
+/// Borrows StringBuilder and create a unique StringSplice that should be
+/// immutable
+StringSplice_t *sb_split_at(StringBuilder_t *builder, char *delim) {
   StringSplice_t *splice = malloc(sizeof(StringSplice_t));
   splice->splice_count = 0;
   splice->delim = 0;
   splice->strsplice = (char **)calloc(STRING_SPLICE_INIT_SIZE, sizeof(char *));
   char *src_cpy = strdup(builder->str);
   char *tok = strtok(src_cpy, delim);
-  while (tok)
-  {
+  while (tok) {
     ++splice->splice_count;
     size_t copytoken_size = strlen(tok);
-    splice->strsplice[splice->splice_count - 1] = malloc(copytoken_size * sizeof(char));
+    splice->strsplice[splice->splice_count - 1] =
+        malloc(copytoken_size * sizeof(char));
     splice->strsplice[splice->splice_count - 1] = strdup(tok);
     tok = strtok(NULL, delim);
   }
--- build.c
+++ build.c
@@ -1,17 +1,40 @@
 #include "CPM.h"
 
-void regen(){
-  cpm_dirops(D_CREATE, "build");
+void regen() {
+  cpm_dirops(D_DELETE, "build");
+  cpm_dirops(D_CREATE, "build/raysrc");
 }
 
-void buildobjects(){
-  cpm_quick_compile("clang", cpm_glob_dir("./srcs", "*.c"), "build", EXECUTABLE, "main", "");
-  pid_t addobj = cpm_quick_compile_async("clang", "./srcs/add.c", "build", OBJECT, "add.o", NULL);
-  cpm_compile_async_poll(addobj);
+void buildobjects() {
+  StringBuilder_t *sb = sb_new();
+  sb_appendstr(sb, cpm_glob_dir("./raysrc", "*.c"));
+  StringSplice_t *srcs = sb_split_at(sb, " ");
+  sb_patsubst(sb, ".c", ".o");
+  StringSplice_t *objs = sb_split_at(sb, " ");
+  sb_free(sb);
+
+  pid_t compilation_proc[srcs->splice_count];
+  for (size_t i = 0; i < srcs->splice_count; i++) {
+    compilation_proc[i] = cpm_quick_compile_async(
+        "clang", srcs->strsplice[i], "build", OBJECT, objs->strsplice[i],
+        "-DPLATFORM_DESKTOP -DUSE_WAYLAND -D_GNU_SOURCE "
+        "-I./raysrc/external/glfw/include ");
+  }
+
+  cpm_compile_async_poll_many(compilation_proc);
 }
 
-int main(int argc, char** argv){
-  cpm_rebuild_self(argv)
+void cpm_build_raylib() {
+  cpm_quick_compile("clang", cpm_glob_dir("./build/raysrc", "*.o"), "build",
+                    DYNLIB, "libraylib.so", NULL);
+}
+void run() { system("LD_LIBRARY_PATH=./build ./build/main"); }
+int main(int argc, char **argv) {
+  cpm_rebuild_self(argv);
   regen();
   buildobjects();
+  cpm_build_raylib();
+  cpm_quick_compile("clang", "./srcs/main.c", "build", EXECUTABLE, "main",
+                    "-I./raysrc -Lbuild -lraylib -lm");
+  run();
 }
